4d0c1274ef250c6d4cd58ef32264542923e4c387
soni240 <s.fegade21@gmail.com>
2022-06-12 00:42:18 +0530

    [Gunavanti]group by gender

--- UC7-groupbygender.sql
+++ UC7-groupbygender.sql
@@ -17,7 +17,7 @@ select * from employee_payroll
 select salary from empoyee_payroll where name='Tanvi';
 select salary from empoyee_payroll where startDate BETWEEN Cast('2020-12-20' as Date) and GetDate();
 
-------- UC 6: Add Gender Column and Update Table Values -------
+--- UC 6: Add Gender Column and Update Table Values ---
 alter table employee_payroll add gender char(1)
 
 Update employee_payroll set gender='R'
@@ -25,3 +25,11 @@ where name='Rakesh';
 Update employee_payroll 
 set gender='F'
 where name='Ankita' or name='Tanvi'
+
+--- UC 7: Use Aggregate Functions and Group by Gender 
+
+select Sum(salary) as "TotalSalary",gender from employee_payroll group by gender;
+select Avg(salary) as "AverageSalary",gender from employee_payroll group by gender;
+select Min(salary) as "MinimumSalary",gender from employee_payroll group by gender;
+select Max(salary) as "MaximumSalary",gender from employee_payroll group by gender;
+select count(salary) as "CountSalary",gender from employee_payroll group by gender;
