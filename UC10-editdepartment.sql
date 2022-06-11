9e3092f64d1300c950f8072ba46240e70c2e14cf
soni240 <s.fegade21@gmail.com>
2022-06-12 00:47:58 +0530

    [Gunavanti]editing department

--- UC10-editdepartment.sql
+++ UC10-editdepartment.sql
@@ -14,3 +14,8 @@ set NetPay=(BasicPay - Deduction)
 update EmployeePayroll
 set TaxablePay=0,IncomeTax=0
 select * from EmpoyeePayroll;
+
+------- UC 10: Adding another Value for ovi in Editing Department -------
+
+Insert into EmployeePayroll(name,BasicPay,StartDate,Address,EmployeePhoneNumber,EmployeeDepartment) values ('Tanvi',250000,'2019-04-20','Chennai,TN','9600054540','Editing');
+select * from EmployeePayroll;
